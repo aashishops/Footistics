8e032bb27a1498537f1833f1c30c99be15b8eb11
Aashish Sekar <107553285+aashishops@users.noreply.github.com>
2023-11-08 06:34:50 +0530

    Update 2_Shot_maps.py

--- src/pages/2_Shot_maps.py
+++ src/pages/2_Shot_maps.py
@@ -31,13 +31,15 @@ selected_player = st.sidebar.selectbox("Select a player:", players)
 
 st.title("Football Pitch Visualizations")
 
-ax = create_full_pitch(x_lims, y_lims, background_color="#0E1117", line_color="white", line_width=2.0)
+ax = create_full_pitch(x_lims=[0, 1.15], y_lims=[0, 0.74], background_color=background_color, line_color=line_color, line_width=line_width)
+
 
 sns.scatterplot(x="X", y="Y", data=shots_df[shots_df["player"] == selected_player], size="xG", ax=ax)
 
 st.pyplot(plt)
 
-ax = create_full_pitch(x_lims, y_lims, background_color="#0E1117", line_color="white", line_width=2.)
+ax = create_full_pitch(x_lims=[0, 1.15], y_lims=[0, 0.74], background_color=background_color, line_color=line_color, line_width=line_width)
+
 
 plot_kdeplot(ax,full_pitch_shots_df, selected_player)
 
