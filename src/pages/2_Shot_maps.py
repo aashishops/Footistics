5f0218c5f00f3d702dcfc73c11bc4e69a4fbf306
Aashish Sekar <107553285+aashishops@users.noreply.github.com>
2023-11-08 06:34:05 +0530

    Update 2_Shot_maps.py

--- src/pages/2_Shot_maps.py
+++ src/pages/2_Shot_maps.py
@@ -58,7 +58,7 @@ line_color = "white"
 line_width = 2.0  
 
 
-ax = create_full_pitch(x_lims, y_lims, background_color="#0E1117", line_color="white", line_width=2.)
+ax = create_full_pitch(x_lims=[0, 1.15], y_lims=[0, 0.74], background_color=background_color, line_color=line_color, line_width=line_width)
 sns.scatterplot(x="X", y="Y", data=data, size="xG", ax=ax)
 
 st.pyplot(plt)
