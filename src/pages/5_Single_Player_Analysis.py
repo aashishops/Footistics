76d715c9c9943b0b211aea26a3d93bd1d6159d46
Aashish Sekar <107553285+aashishops@users.noreply.github.com>
2023-11-15 09:54:53 +0530

    Update 5_Single_Player_Analysis.py

--- src/pages/5_Single_Player_Analysis.py
+++ src/pages/5_Single_Player_Analysis.py
@@ -1,7 +1,7 @@
 from comparison import single_player_comparison
 import pandas as pd
 import streamlit as st 
-from abbrevation import fb_df,dm_df,gk_df,cm_df,am_df,striker_df,cb_df,wi_df,position_to_abbreviations
+from abbrevation import fb,dm,gk,cm,am,striker,cb,wi,position_to_abbreviations
 st.set_page_config(
     page_title="Footistcs",
     page_icon="⚽",
