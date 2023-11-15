4121effe7a750216b7e3cb445032027e1d941367
Aashish Sekar <107553285+aashishops@users.noreply.github.com>
2023-11-15 09:54:06 +0530

    Update 5_Single_Player_Analysis.py

--- src/pages/5_Single_Player_Analysis.py
+++ src/pages/5_Single_Player_Analysis.py
@@ -1,24 +1,23 @@
 from comparison import single_player_comparison
 import pandas as pd
-import streamlit as st
-from abbrevation import fb_df, dm_df, gk_df, cm_df, am_df, striker_df, cb_df, wi_df
-
+import streamlit as st 
+from abbrevation import fb_df,dm_df,gk_df,cm_df,am_df,striker_df,cb_df,wi_df,position_to_abbreviations
 st.set_page_config(
     page_title="Footistcs",
     page_icon="⚽",
 )
 
-# Sample DataFrames and dictionary for testing
-fb_df = pd.read_csv(r"FInal_DF/fb.csv")
-dm_df = pd.read_csv(r"FInal_DF/dm.csv")
-gk_df = pd.read_csv(r"FInal_DF/gk.csv")
-cm_df = pd.read_csv(r"FInal_DF/cm.csv")
-am_df = pd.read_csv(r"FInal_DF/am.csv")
-striker_df = pd.read_csv(r"FInal_DF/strikers.csv")
-cb_df = pd.read_csv(r"FInal_DF/cb.csv")
-wi_df = pd.read_csv(r"FInal_DF/wi.csv")
-ps_df = pd.read_csv(r"Web scrapping/Final Data/position.csv")
 
+# Sample DataFrames and dictionary for testing
+fb_df=pd.read_csv(r"FInal_DF/fb.csv")
+dm_df=pd.read_csv(r"FInal_DF/dm.csv")
+gk_df=pd.read_csv(r"FInal_DF/gk.csv")
+cm_df=pd.read_csv(r"FInal_DF/cm.csv")
+am_df=pd.read_csv(r"FInal_DF/am.csv")
+striker_df=pd.read_csv(r"FInal_DF/strikers.csv")
+cb_df=pd.read_csv(r"FInal_DF/cb.csv")
+wi_df=pd.read_csv(r"FInal_DF/wi.csv")
+ps_df=pd.read_csv(r"Web scrapping/Final Data/position.csv")
 # Create a dictionary to map player positions to their respective DataFrames
 position_to_df = {
     'Right-Back': fb_df,
@@ -45,8 +44,7 @@ with st.sidebar:
 # Get the player's position from the DataFrame
 player_position = ps_df.loc[ps_df["Player"] == player_name, "Position"].iloc[0]
 
-# Get the corresponding abbreviation dictionary for the player's position
-abbreviation_dict = position_to_df.get(player_position, {})
+abbreviation_dict = position_to_abbreviations.get(player_position, {})
 
 # Perform the rest of your code for single player comparison
 fig = single_player_comparison(player_name, ps_df, position_to_df)
