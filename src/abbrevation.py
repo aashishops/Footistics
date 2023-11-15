dfba5c6dd25b39d744699edb40c4ea31405544ac
Aashish Sekar <107553285+aashishops@users.noreply.github.com>
2023-11-15 09:38:04 +0530

    Create abbrevation.py

---
+++ src/abbrevation.py
@@ -0,0 +1,195 @@
+fb_df = {
+    'BTklInt': 'Ball Tackles and Interceptions',
+    'CTkl%': 'Successful Tackle Percentage',
+    'PTCrs': 'Crosses from Penalty Area',
+    'PTTB': 'Passes to Target in Box',
+    'SCA90': 'Shot-Creating Actions per 90',
+    'LPrgP': 'Passes into the Final Third',
+    'LFTP': 'Final Third Passes',
+    'LxA': 'Assists from Live-Ball Passes',
+    'CaFTC': 'Chances Created from Open Play',
+    'CaPrgC': 'Progressive Passes Leading to a Chance',
+    'Def_3rd': 'Actions in Defensive Third',
+    'Mid_3rd': 'Actions in Middle Third',
+    'Att_3rd': 'Actions in Attacking Third',
+    'Live': 'Live-Ball Actions',
+    'PrgR': 'Progressive Passes Received'
+}
+
+dm_df = {
+    'TCmp': 'Total Passes Completed',
+    'LFTP': 'Final Third Passes',
+    'LPrgP': 'Passes into the Final Third',
+    'Def_3rd': 'Actions in Defensive Third',
+    'Mid_3rd': 'Actions in Middle Third',
+    'Att_3rd': 'Actions in Attacking Third',
+    'Live': 'Live-Ball Actions',
+    'TOSucc': 'Successful Take-Ons',
+    'CaPrgC': 'Progressive Passes Leading to a Chance',
+    'PrgR': 'Progressive Passes Received',
+    'BTklInt': 'Ball Tackles and Interceptions',
+    'CTkl%': 'Successful Tackle Percentage',
+    'BClr': 'Blocks Cleared',
+    'ADWon': 'Aerial Duels Won',
+    'ADWonp': 'Aerial Duels Won Percentage'
+}
+
+gk_df = {
+    'PSxG': 'Post-Shot Expected Goals',
+    'PSxG/SoT': 'Post-Shot xG per Shot on Target',
+    'PSxG-GA_p90': 'Post-Shot xG - Goals Against per 90',
+    'LCmp': 'Long Passes Completed',
+    'LAtt': 'Long Passes Attempted',
+    'PaAtt': 'Passes Attempted',
+    'PaThr': 'Passes Thrown',
+    'PaAvgLen': 'Average Pass Length',
+    'GKAtt': 'Goalkeeper Actions',
+    'GKAvgLen': 'Average Goalkeeper Action Length',
+    'COpp': 'Crosses Opposed',
+    'CStp': 'Crosses Stopped',
+    'SwOPA_p90': 'Sweeper Actions per 90',
+    'SoTA': 'Shots on Target Against',
+    'Saves': 'Saves',
+    'Savep': 'Save Percentage',
+    'CS': 'Clean Sheets',
+    'PKsv': 'Penalty Saves'
+}
+
+cm_df = {
+    'TCmp': 'Total Passes Completed',
+    'LFTP': 'Final Third Passes',
+    'LPrgP': 'Passes into the Final Third',
+    'Def_3rd': 'Actions in Defensive Third',
+    'Mid_3rd': 'Actions in Middle Third',
+    'Att_3rd': 'Actions in Attacking Third',
+    'Live': 'Live-Ball Actions',
+    'TOSucc': 'Successful Take-Ons',
+    'CaPrgC': 'Progressive Passes Leading to a Chance',
+    'PrgR': 'Progressive Passes Received',
+    'TOAtt': 'Take-On Attempts',
+    'BTklInt': 'Ball Tackles and Interceptions',
+    'PTTB': 'Passes to Target in Box'
+}
+
+am_df = {
+    'TCmp': 'Total Passes Completed',
+    'LFTP': 'Final Third Passes',
+    'LPrgP': 'Passes into the Final Third',
+    'LPPA': 'Passes into the Penalty Area',
+    'LxA': 'Assists from Live-Ball Passes',
+    'Def_3rd': 'Actions in Defensive Third',
+    'Mid_3rd': 'Actions in Middle Third',
+    'Att_3rd': 'Actions in Attacking Third',
+    'Live': 'Live-Ball Actions',
+    'TOSucc': 'Successful Take-Ons',
+    'CaPrgC': 'Progressive Passes Leading to a Chance',
+    'PrgR': 'Progressive Passes Received',
+    'CaFTC': 'Chances Created from Open Play',
+    'CaCPA': 'Chances Created from Penalty Area',
+    'Att_Pen': 'Actions in Penalty Area',
+    'SSh_p90': 'Shots per 90',
+    'npxG': 'Non-Penalty Expected Goals',
+    'SDist': 'Shot Distance',
+    'PTTB': 'Passes to Target in Box'
+}
+
+striker_df = {
+    'xAG_p90': 'Expected Assists per 90',
+    'LPPA': 'Passes into the Penalty Area',
+    'CaCPA': 'Chances Created from Penalty Area',
+    'NPxG_p90': 'Non-Penalty Expected Goals per 90',
+    'npxG/Sh': 'Non-Penalty Expected Goals per Shot',
+    'SSh_p90': 'Shots per 90',
+    'SSoT_90': 'Shots on Target per 90',
+    'ADWon': 'Aerial Duels Won',
+    'ADWonp': 'Aerial Duels Won Percentage',
+    'LPrgP_x': 'Passes into the Final Third',
+    'SCA90': 'Shot-Creating Actions per 90',
+    'LKP': 'Key Passes',
+    'Off': 'Offsides',
+    'SDist': 'Shot Distance',
+    'LxA': 'Assists from Live-Ball Passes',
+    'Att_Pen': 'Actions in Penalty Area',
+    'LPrgP_y': 'Passes into the Final Third',
+    'LFTP': 'Final Third Passes',
+    'Mid_3rd': 'Actions in Middle Third',
+    'Att_3rd': 'Actions in Attacking Third',
+    'Live': 'Live-Ball Actions'
+}
+
+cb_df = {
+    'BTklInt': 'Ball Tackles and Interceptions',
+    'CTkl': 'Total Tackles',
+    'TCmp': 'Total Passes Completed',
+    'LCmp': 'Long Passes Completed',
+    'LAtt': 'Long Passes Attempted',
+    'CaPrgC': 'Progressive Passes Leading to a Chance',
+    'LPrgP': 'Passes into the Final Third',
+    'LFTP': 'Final Third Passes',
+    'PrgC': 'Progressive Carries',
+    'PrgP': 'Progressive Passes',
+    'BClr': 'Blocks Cleared',
+    'BSh': 'Blocked Shots',
+    'BPass': 'Ball Passes',
+    'Def_3rd': 'Actions in Defensive Third',
+    'Mid_3rd': 'Actions in Middle Third',
+    'Att_3rd': 'Actions in Attacking Third',
+    'Live': 'Live-Ball Actions',
+    'ADWonp': 'Aerial Duels Won Percentage',
+    'ADWon': 'Aerial Duels Won'
+}
+
+wi_df = {
+    'PTCrs': 'Penalty Area Crosses',
+    'PTTB': 'Passes to Target in Box',
+    'LxA': 'Assists from Live-Ball Passes',
+    'LKP': 'Key Passes',
+    'LFTP': 'Final Third Passes',
+    'LPPA': 'Passes into the Penalty Area',
+    'LPrgP': 'Passes into the Final Third',
+    'Att_Pen': 'Actions in Penalty Area',
+    'TOAtt': 'Take-On Attempts',
+    'TOSucc': 'Successful Take-Ons',
+    'CaPrgC': 'Progressive Passes Leading to a Chance',
+    'CaCPA': 'Chances Created from Penalty Area',
+    'PrgR': 'Progressive Passes Received',
+    'npxG': 'Non-Penalty Expected Goals',
+    'npxG/Sh': 'Non-Penalty Expected Goals per Shot',
+    'SSh_p90': 'Shots per 90',
+    'SSoT_90': 'Shots on Target per 90',
+    'SDist': 'Shot Distance',
+    'Off': 'Offsides'
+}
+
+# Create a dictionary to map player positions to their respective DataFrames
+position_to_df = {
+    'Right-Back': fb_df,
+    'Left-Back': fb_df,
+    'Left WingBack': fb_df,
+    'Right WingBack': fb_df,
+    'Defensive Midfield': dm_df,
+    'Goalkeeper': gk_df,
+    'Central Midfield': cm_df,
+    'Attacking Midfield': am_df,
+    'Centre-Forward': striker_df,
+    'Second Striker': striker_df,
+    'Centre-Back': cb_df,
+    'Right Winger': wi_df,
+    'Left Winger': wi_df
+}
+
+position_to_df = {
+    'Right-Back': fb_df,
+    'Left-Back': fb_df,
+    'Left WingBack': fb_df,
+    'Right WingBack': fb_df,
+    'Defensive Midfield': dm_df,
+    'Goalkeeper': gk_df,
+    'Central Midfield': cm_df,
+    'Attacking Midfield': am_df,
+    'Centre-Forward': striker_df,
+    'Second Striker': striker_df,
+    'Centre-Back': cb_df,
+    'Right Winger': wi_df,
+    'Left Winger': wi_df
+}
