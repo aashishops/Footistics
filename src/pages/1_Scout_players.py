843cc865850ef13a6cbc6892d66ddfc5246ac6ce
Aashish Sekar <107553285+aashishops@users.noreply.github.com>
2023-11-15 10:12:34 +0530

    Update 1_Scout_players.py

--- src/pages/1_Scout_players.py
+++ src/pages/1_Scout_players.py
@@ -1,25 +1,22 @@
 import streamlit as st
 import pandas as pd
 from Scout_player import find_and_plot_similar_players  
-from abbrevation import fb,dm,gk,cm,am,striker,cb,wi,position_to_abbrevations
-
+from abbrevation import fb, dm, gk, cm, am, striker, cb, wi, position_to_abbrevations
 
 # Create a Streamlit app
 st.title("Find Similar Players")
 
+fb_df = pd.read_csv(r"FInal_DF/fb.csv")
+dm_df = pd.read_csv(r"FInal_DF/dm.csv")
+gk_df = pd.read_csv(r"FInal_DF/gk.csv")
+cm_df = pd.read_csv(r"FInal_DF/cm.csv")
+am_df = pd.read_csv(r"FInal_DF/am.csv")
+striker_df = pd.read_csv(r"FInal_DF/strikers.csv")
+cb_df = pd.read_csv(r"FInal_DF/cb.csv")
+wi_df = pd.read_csv(r"FInal_DF/wi.csv")
+ps_df = pd.read_csv(r"Web scrapping/Final Data/position.csv")
 
-fb_df=pd.read_csv(r"FInal_DF/fb.csv")
-dm_df=pd.read_csv(r"FInal_DF/dm.csv")
-gk_df=pd.read_csv(r"FInal_DF/gk.csv")
-cm_df=pd.read_csv(r"FInal_DF/cm.csv")
-am_df=pd.read_csv(r"FInal_DF/am.csv")
-striker_df=pd.read_csv(r"FInal_DF/strikers.csv")
-cb_df=pd.read_csv(r"FInal_DF/cb.csv")
-wi_df=pd.read_csv(r"FInal_DF/wi.csv")
-ps_df=pd.read_csv(r"Web scrapping/Final Data/position.csv")
-
-
-player_names = player_df['Player'].tolist()
+player_names = ps_df['Player'].tolist()
 player_name = st.sidebar.selectbox("Select a player:", player_names)
 
 position_to_df = {
@@ -42,7 +39,6 @@ player_position = ps_df.loc[ps_df["Player"] == player_name, "Position"].iloc[0]
 
 abbreviation_dict = position_to_abbrevations.get(player_position, {})
 
-
 if player_name:
     similar_players, radar_plot = find_and_plot_similar_players(position_to_df, player_name)
     
@@ -53,3 +49,7 @@ if player_name:
 
     with col2:
         st.plotly_chart(radar_plot)
+
+    st.write(f"\nAbbreviations for {player_position}:\n")
+    for key, value in abbreviation_dict.items():
+        st.write(f"{key} - {value}")
